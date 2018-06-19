a5a9e932f29b4ca6ae1e70a37800abcb814990f4
Clodoaldo Elias <clodoaldoelias@gmail.com>
2018-06-19 20:10:19 -0300

    Estado e cidade

---
+++ src/main/java/com/cldelias/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.cldelias.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cldelias.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository  extends JpaRepository<Cidade, Integer>{
+
+}
