6e49007aff5dfba5714fcea8de450a980d913921
Clodoaldo Elias <clodoaldoelias@gmail.com>
2018-06-21 21:18:49 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/cldelias/cursomc/resources/ClienteResource.java
@@ -0,0 +1,29 @@
+package com.cldelias.cursomc.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cldelias.cursomc.domain.Cliente;
+import com.cldelias.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Cliente obj = this.service.buscar(id);
+		return ResponseEntity.ok(obj);
+	}
+
+}
